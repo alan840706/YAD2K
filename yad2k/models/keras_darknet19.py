1c69b348910ad1c90c9c52ad6c5bbbf2eb3bff46
alan840706 <58114247+alan840706@users.noreply.github.com>
2020-09-27 20:01:20 +0800

    Update keras_darknet19.py

--- yad2k/models/keras_darknet19.py
+++ yad2k/models/keras_darknet19.py
@@ -4,6 +4,7 @@ from functools import partial
 
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.activation import ReLU
 from keras.layers.normalization import BatchNormalization
 from keras.models import Model
 from keras.regularizers import l2
@@ -31,6 +32,14 @@ def DarknetConv2D_BN_Leaky(*args, **kwargs):
         BatchNormalization(),
         LeakyReLU(alpha=0.1))
 
+def DarknetConv2D_BN_Leaky(*args, **kwargs):
+    """Darknet Convolution2D followed by BatchNormalization and LeakyReLU."""
+    no_bias_kwargs = {'use_bias': False}
+    no_bias_kwargs.update(kwargs)
+    return compose(
+        DarknetConv2D(*args, **no_bias_kwargs),
+        BatchNormalization(),
+        LeakyReLU(alpha=0.1))
 
 def bottleneck_block(outer_filters, bottleneck_filters):
     """Bottleneck block of 3x3, 1x1, 3x3 convolutions."""
