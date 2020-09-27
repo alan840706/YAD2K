a955f580bb4361d66d13f3aea18c560b93e01254
alan840706 <58114247+alan840706@users.noreply.github.com>
2020-09-28 02:17:25 +0800

    Update test_yolo.py

--- test_yolo.py
+++ test_yolo.py
@@ -7,7 +7,7 @@ import os
 import random
 
 import numpy as np
-from keras import backend as K
+import keras.backend.tensorflow_backend as K
 from keras.models import load_model
 from PIL import Image, ImageDraw, ImageFont
 
